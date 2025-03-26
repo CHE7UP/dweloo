8562d9acba6f41a9bb0af88d1f2c0ae7d4f69b6f
Alexei Bucos <bucos@Alexeis-MacBook-Pro-2.local>
2025-03-25 23:36:39 -0700

    linksfooter

--- src/app/components/QuoteMockup2.tsx
+++ src/app/components/QuoteMockup2.tsx
@@ -11,25 +11,25 @@ const QuoteMockup2: React.FC = () => {
         direction="top"
         distance="12"
         easing="ease-in-out"
-        className="w-[170px] h-[85px] rounded-[20px] shadow-lg p-3 bg-white flex flex-col justify-between border border-gray-200 absolute top-4/8 left-35 right-2 z-10 -translate-y-1/2" 
+        className="w-[170px] lg:w-[250px] h-[95px] lg:h-[125px] rounded-[20px] shadow-lg p-3 bg-white flex flex-col justify-between border border-gray-200 absolute top-4/8 right-1 z-10 -translate-y-1/2" 
         >
         {/* Header */}
         <div className="flex items-center justify-between mb-1">
-            <Avatar className="w-5 h-5">
+            <Avatar className="w-5 h-5 lg:w-6 lg:h-6">
             <AvatarImage src="/assets/img/landing-page-assets/avatar-head-dweloo.png" />
             <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-            <span className="text-[7px] text-[#1976D2] font-medium">Booking #420-69</span>
+            <span className="text-[7px] lg:text-[11px] text-[#1976D2] font-medium">Booking #420-69</span>
         </div>
         
         {/* Content */}
         <div className="space-y-0.5 flex-grow">
             <div className="flex items-center justify-between">
-            <span className="font-semibold text-[#1976D2] text-[9px]">Installation Confirmed</span>
+            <span className="font-semibold text-[#1976D2] text-[9px] mb-1 lg:text-[11px]">Installation Confirmed</span>
             <CheckCircle className="w-3 h-3 text-[#1976D2]" /> {/* Lucide React icon */}
             </div>
-            <div className="text-[6.5px] text-gray-700 leading-tight">
-            <p>98115, Seattle, WA, USA</p>
+            <div className="text-[6.5px] lg:text-[9px] text-gray-700 mb-1">
+            <p>Seattle, WA, 98115</p>
             <p>
             {new Date(Date.now() + 2 * 24 * 60 * 60 * 1000).toLocaleDateString('en-US', {
                 month: 'short',
--- src/app/components/QuoteMockup3.tsx
+++ src/app/components/QuoteMockup3.tsx
@@ -11,36 +11,36 @@ const QuoteMockup3: React.FC = () => {
   direction="top"
   distance="12"
   easing="ease-in-out"
-  className="w-[170px] h-[110px] rounded-[20px] shadow-lg p-3 bg-white flex flex-col justify-between border border-gray-200 absolute top-5/7 left-35 right-1 z-10 -translate-y-1/2"
+  className="w-[170px] lg:w-[250px] h-[110px] lg:h-[145px] rounded-[20px] shadow-lg p-3 bg-white flex flex-col justify-between border border-gray-200 absolute top-5/7 right-1 z-10 -translate-y-1/2"
 >
   {/* Header */}
   <div className="flex items-center justify-between mb-1">
     <Avatar className="w-5 h-5">
       <AvatarImage 
-      className="w-4 h-4 object-contain"
+      className="w-4 h-4 lg:w-6 lg:h-6 object-contain"
       src="/favicons/favicon-96x96.png" /> Replace with Dweloo logo
       <AvatarFallback>DW</AvatarFallback>
     </Avatar>
-    <span className="text-[7px] text-[#1976D2] font-medium">Invoice #420-69</span>
+    <span className="text-[7px] lg:text-[10px] text-[#1976D2] font-medium">Invoice #420-69</span>
   </div>
   
   {/* Content */}
   <div className="space-y-1 flex-grow">
     <div className="flex items-center justify-between">
-      <span className="font-semibold text-[#1976D2] text-[9px]">Job Completed</span>
+      <span className="font-semibold text-[#1976D2] text-[9px] lg:text-[13px]">Job Completed</span>
       <CheckCircle className="w-3 h-3 text-[#1976D2]" />
     </div>
 
     <div className="flex items-center space-x-0.5">
       {[...Array(5)].map((_, i) => (
-        <Star key={i} className="w-2 h-2 text-yellow-400 fill-yellow-400" />
+        <Star key={i} className="w-2 h-2 lg:w-3 lg:h-3 text-yellow-400 fill-yellow-400" />
       ))}
-      <span className="text-[6px] text-gray-600 ml-1">5/5</span>
+      <span className="text-[6px] lg:text-[9px] text-gray-600 ml-1">5/5</span>
     </div>
-    <div className="text-[6.5px] text-gray-700 leading-tight">
+    <div className="text-[6.5px] lg:text-[11px] text-gray-700 leading-tight">
       <p>Thanks for the review!</p>
     </div>
-    <button className="w-full bg-[#1976D2] text-white text-[6px] py-1 rounded-md hover:bg-[#1565c0]">
+    <button className="w-full bg-[#1976D2] text-white text-[9px] lg:text-[13px] py-1 lg:mt-3 rounded-md hover:bg-[#1565c0]">
       View Invoice
     </button>
   </div>
