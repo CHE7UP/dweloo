7ec7d9d7dae0cb5500c96adb70b80f8cec4d1a8b
Alexei Bucos <bucos@Alexeis-MacBook-Pro-2.local>
2025-04-13 23:58:24 -0700

    123wer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -126,14 +126,17 @@ export default function RootLayout({
           `}
         </Script>
         
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        {/* Google Tag Manager Script */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17005771269"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-XXXXXXX');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17005771269');
           `}
         </Script>
         
